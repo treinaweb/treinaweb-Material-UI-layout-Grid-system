15f44b5f7893e211af2d4335e1b22056a2a9c06c
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2022-09-28 16:49:15 -0300

    02:04 - Conhecendo Grid container e Grid item

--- src/App.js
+++ src/App.js
@@ -1,32 +1,40 @@
 import Container from '@mui/material/Container'
+import Grid from '@mui/material/Grid'
+
+function Paragrafo() {
+  return (
+    <p>
+      Est commodo aute duis ad voluptate. Aute proident ex officia ea pariatur id sit ipsum nisi nisi
+      adipisicing. Amet in et labore irure adipisicing. Tempor commodo commodo exercitation dolore.
+      Dolore dolor dolore et magna. In et consectetur laborum ex esse aliqua culpa ut. Sunt sint labore
+      proident consequat.
+    </p>
+  )
+}
+
 function App() {
   return (
     <Container >
       <h1>Exemplo</h1>
-      <p>
-        Est commodo aute duis ad voluptate. Aute proident ex officia ea pariatur id sit ipsum nisi nisi
-        adipisicing. Amet in et labore irure adipisicing. Tempor commodo commodo exercitation dolore.
-        Dolore dolor dolore et magna. In et consectetur laborum ex esse aliqua culpa ut. Sunt sint labore
-        proident consequat.
-      </p>
-      <p>
-        Est commodo aute duis ad voluptate. Aute proident ex officia ea pariatur id sit ipsum nisi nisi
-        adipisicing. Amet in et labore irure adipisicing. Tempor commodo commodo exercitation dolore.
-        Dolore dolor dolore et magna. In et consectetur laborum ex esse aliqua culpa ut. Sunt sint labore
-        proident consequat.
-      </p>
-      <p>
-        Est commodo aute duis ad voluptate. Aute proident ex officia ea pariatur id sit ipsum nisi nisi
-        adipisicing. Amet in et labore irure adipisicing. Tempor commodo commodo exercitation dolore.
-        Dolore dolor dolore et magna. In et consectetur laborum ex esse aliqua culpa ut. Sunt sint labore
-        proident consequat.
-      </p>
-      <p>
-        Est commodo aute duis ad voluptate. Aute proident ex officia ea pariatur id sit ipsum nisi nisi
-        adipisicing. Amet in et labore irure adipisicing. Tempor commodo commodo exercitation dolore.
-        Dolore dolor dolore et magna. In et consectetur laborum ex esse aliqua culpa ut. Sunt sint labore
-        proident consequat.
-      </p>
+      <Grid container spacing={4}>
+
+        <Grid item xs={6}>
+          <Paragrafo />
+        </Grid>
+
+        <Grid item xs={6}>
+          <Paragrafo />
+        </Grid>
+
+        <Grid item xs={12}>
+          <Paragrafo />
+        </Grid>
+
+        <Grid item xs={8}>
+          <Paragrafo />
+        </Grid>
+
+      </Grid>
     </Container>
   );
 }
